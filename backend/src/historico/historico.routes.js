aa173473484fb083425674a39104539e5b02a01e
Fabiana Souza de Paula <fabisouza14112005@gmail.com>
2026-06-19 19:32:24 -0300

    Delete backend/src/historico directory

--- backend/src/historico/historico.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const HistoricoController = require('./historico.controller');
-
-router.post('/', HistoricoController.salvarVenda);
-router.get('/', HistoricoController.listarVendas);
-
-module.exports = router;
