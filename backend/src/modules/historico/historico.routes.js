71dadedfbd69c176ca91ee805b303ddf02cbe7b5
Fabiana Souza de Paula <fabisouza14112005@gmail.com>
2026-06-19 19:30:05 -0300

    Add files via upload

---
+++ backend/src/modules/historico/historico.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const HistoricoController = require('./historico.controller');
+
+router.post('/', HistoricoController.salvarVenda);
+router.get('/', HistoricoController.listarVendas);
+
+module.exports = router;
